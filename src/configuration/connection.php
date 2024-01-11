4e92385d8bc252bb8fed18167f1450e34df1a58a
Luiz Henrique <luiz.jose@a.unileste.edu.br>
2024-01-11 15:20:53 -0300

    Instalação de dependencias com composer

---
+++ src/configuration/connection.php
@@ -0,0 +1,36 @@
+<?php
+
+define('HOST','localhost');
+define('DATABASE','crud-mvc-php-poo');
+define('USER','root');
+define('PASSWORD','');
+
+class Connect{
+    protected $connection;
+
+    function __construct()
+    {
+        $this->connectDataBase();
+    }
+
+    function connectDataBase()
+    {
+
+        try{
+            $this->connection = new PDO('mysql:host='.HOST.';dbaname='.DATABASENAME,USER,PASSWORD);
+           }
+
+           catch(PDOException)
+           {
+            echo"Error". $e->getMessage();
+            die();
+           }
+
+        }
+     }
+
+     $testConnection = new Connection;
+     
+    
+
+?>
